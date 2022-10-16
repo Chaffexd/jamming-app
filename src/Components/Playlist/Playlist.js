7f0ad11e562488457a0edc209b39f50860d9abb7
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-10-16 08:15:30 +0400

    Update
    Added the functionality to add a track to the track playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,7 +5,7 @@ import TrackList from '../TrackList/TrackList';
 class Playlist extends React.Component {
     render() {
         return(
-            <div class="Playlist">
+            <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
                 <TrackList tracks={ this.props.playlistTracks }/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
